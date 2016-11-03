775f075c059bab63433c70d664bdd0597949ea4c
Fabricio C Zuardi <fabricio@fabricio.org>
2016-11-03 19:08:22 -0200

    API breaking change: will simply read whatever range and add to sheets.sheetname

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const extend = require('xtend');
 const gsheets = google.sheets('v4');
 
 // Setup a middleware that will read a range of values from a
-// Google Sheets Spreadsheet and populate a "sheet" property
-// in the ctx.state
+// Google Sheets Spreadsheet range and add the values in the context state
 //
-// Arguments:
+// ### Arguments:
 // - params: should be an object containing:
 //   - auth: an oauth2Client
 //   - spreadsheetId: the id of the spreadsheet, the part after /d/... in the sheet url
 //   - range: a range to read values from, example: 'Sheet1!A1:K'
-// - withHead: default = true, a boolean to flag if the first row of a range
-// is the head of a table with titles and not data
 //
-// Modified ctx.state:
+// ### Modified ctx.state:
 // If the read is sucessful this middleware will add a new object on
-// ```ctx.state.sheet``` containing the properties ```head``` and ```body```,
-// both are arrays of rows, ```head``` will be null if the withHead flag was false
-const createMiddleWare = ({ params, withHead = true }) => (ctx, next) => {
+// ```ctx.state.sheets[sheetName]``` containing the values of the range,
+// sheetName is the name of the sheet, for example, if the range is
+// `Sheet1!A1:k' the values will be accessible under ```ctx.sheets['Sheet1']```
+const createMiddleWare = params => (ctx, next) => {
+    const pattern = new RegExp('([^!]*)!');
+    const matches = params.range.match(pattern);
+    const sheetName = matches ? matches[1] : 'Sheet1';
     gsheets.spreadsheets.values.get(params, (err, response) => {
         if (err) {
             console.error(err);
             return err;
         }
-        const sheet =
-            { head: withHead ? response.values[0] : null
-            , body: response.values.slice(1)
-            };
-        const nextState = extend(ctx.state, { sheet });
+        const sheet = response.values;
+        const stateSheets = ctx.state.sheets || {};
+        const nextSheets = extend(stateSheets, { [sheetName]: sheet });
+        const nextState = extend(ctx.state, { sheets: nextSheets });
         ctx.state = nextState; // eslint-disable-line
         return next();
     });
